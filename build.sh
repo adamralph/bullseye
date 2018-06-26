ce8457dbc09f8932e6306803f90d1019a795c2af
Adam Ralph <adam@adamralph.com>
2018-06-26 13:25:42 +0200

    x-plat build

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "${0##*/}": Building...
+dotnet build --configuration Release
+
+echo "${0##*/}": Testing...
+dotnet test ./BullseyeTests/BullseyeTests.csproj --configuration Release --no-build
+
+echo "${0##*/}": Smoke testing...
+trap `set +x` EXIT
+set -x
+dotnet run -c Release --project BullseyeSmokeTester -- --help
+dotnet run -c Release --project BullseyeSmokeTester -- --list-targets
+dotnet run -c Release --project BullseyeSmokeTester -- --list-dependencies
+dotnet run -c Release --project BullseyeSmokeTester --
+dotnet run -c Release --project BullseyeSmokeTester -- --dry-run
+dotnet run -c Release --project BullseyeSmokeTester -- --skip-dependencies
+dotnet run -c Release --project BullseyeSmokeTester -- --dry-run --skip-dependencies
