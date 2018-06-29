72fcb96dbeaacdabec876ef4e04fa26670170148
Adam Ralph <adam@adamralph.com>
2018-06-29 22:27:12 +0200

    encapsulate targets running in TargetCollection

--- Bullseye/Internal/TargetCollectionExtensions.cs
+++ Bullseye/Internal/TargetCollectionExtensions.cs
@@ -2,7 +2,6 @@ namespace Bullseye.Internal
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -93,58 +92,7 @@ namespace Bullseye.Internal
                 names.Add("default");
             }
 
-            var log = new Logger(console, options, palette);
-
-            await log.Running(names).ConfigureAwait(false);
-            var stopWatch = Stopwatch.StartNew();
-
-            try
-            {
-                await RunAsync(targets, names, options.SkipDependencies, options.DryRun, log).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                await log.Failed(names, ex, stopWatch.Elapsed.TotalMilliseconds).ConfigureAwait(false);
-                throw;
-            }
-
-            await log.Succeeded(names, stopWatch.Elapsed.TotalMilliseconds).ConfigureAwait(false);
-        }
-
-        private static async Task RunAsync(TargetCollection targets, List<string> names, bool skipDependencies, bool dryRun, Logger log)
-        {
-            if (!skipDependencies)
-            {
-                targets.ValidateDependencies();
-            }
-
-            targets.Validate(names);
-
-            var targetsRan = new HashSet<string>();
-            foreach (var name in names)
-            {
-                await targets.RunAsync(name, skipDependencies, dryRun, targetsRan, log).ConfigureAwait(false);
-            }
-        }
-
-        private static async Task RunAsync(this TargetCollection targets, string name, bool skipDependencies, bool dryRun, ISet<string> targetsRan, Logger log)
-        {
-            if (!targetsRan.Add(name))
-            {
-                return;
-            }
-
-            var target = targets[name];
-
-            if (!skipDependencies)
-            {
-                foreach (var dependency in target.Dependencies)
-                {
-                    await targets.RunAsync(dependency, skipDependencies, dryRun, targetsRan, log).ConfigureAwait(false);
-                }
-            }
-
-            await target.RunAsync(dryRun, log).ConfigureAwait(false);
+            await targets.RunAsync(names, options.SkipDependencies, options.DryRun, new Logger(console, options, palette));
         }
 
         private static string ToListString(this TargetCollection targets)
@@ -171,46 +119,6 @@ namespace Bullseye.Internal
             }
 
             return value.ToString();
-        }
-
-        private static void ValidateDependencies(this TargetCollection targets)
-        {
-            var unknownDependencies = new SortedDictionary<string, SortedSet<string>>();
-
-            foreach (var target in targets)
-            {
-                foreach (var dependency in target.Dependencies
-                    .Where(dependency => !targets.Contains(dependency)))
-                {
-                    (unknownDependencies.TryGetValue(dependency, out var set)
-                            ? set
-                            : unknownDependencies[dependency] = new SortedSet<string>())
-                        .Add(target.Name);
-                }
-            }
-
-            if (!unknownDependencies.Any())
-            {
-                return;
-            }
-
-            var message = $"Missing {(unknownDependencies.Count > 1 ? "dependencies" : "dependency")} detected: " +
-                string.Join(
-                    "; ",
-                    unknownDependencies.Select(missingDependency =>
-                        $@"{missingDependency.Key.Quote()}, required by {missingDependency.Value.Quote()}"));
-
-            throw new Exception(message);
-        }
-
-        private static void Validate(this TargetCollection targets, List<string> names)
-        {
-            var unknownNames = new SortedSet<string>(names.Except(targets.Select(target => target.Name)));
-            if (unknownNames.Any())
-            {
-                var message = $"The following target{(unknownNames.Count > 1 ? "s were" : " was")} not found: {unknownNames.Quote()}.";
-                throw new Exception(message);
-            }
         }
 
         public static string GetUsage(Palette p) =>
