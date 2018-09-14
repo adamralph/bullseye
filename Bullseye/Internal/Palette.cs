b9835e4cb62ee466d21926e78a9970614c3d56cc
Adam Ralph <adam@adamralph.com>
2018-09-14 17:07:55 +0200

    avoid using background colour for output

--- Bullseye/Internal/Palette.cs
+++ Bullseye/Internal/Palette.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Bullseye.Internal
 {
     public class Palette
@@ -5,20 +7,40 @@ namespace Bullseye.Internal
         public Palette(bool noColor)
         {
             this.Default = noColor ? "" : "\x1b[0m";
+            this.Red= noColor ? "" : "\x1b[31m";
             this.Green = noColor ? "" : "\x1b[32m";
+            this.Yellow = noColor ? "" : "\x1b[33m";
             this.Magenta = noColor ? "" : "\x1b[35m";
             this.Cyan = noColor ? "" : "\x1b[36m";
             this.White = noColor ? "" : "\x1b[37m";
             this.BrightRed = noColor ? "" : "\x1b[91m";
+            this.BrightGreen = noColor ? "" : "\x1b[92m";
             this.BrightYellow = noColor ? "" : "\x1b[93m";
             this.BrightMagenta = noColor ? "" : "\x1b[95m";
             this.BrightCyan = noColor ? "" : "\x1b[96m";
             this.BrightWhite = noColor ? "" : "\x1b[97m";
+
+            this.Red = Console.BackgroundColor == ConsoleColor.DarkRed ? this.BrightRed : this.Red;
+            this.Green = Console.BackgroundColor == ConsoleColor.DarkGreen ? this.BrightGreen : this.Green;
+            this.Yellow = Console.BackgroundColor == ConsoleColor.DarkYellow ? this.BrightYellow : this.Yellow;
+            this.Magenta = Console.BackgroundColor == ConsoleColor.DarkMagenta ? this.BrightMagenta : this.Magenta;
+            this.Cyan = Console.BackgroundColor == ConsoleColor.DarkCyan ? this.BrightCyan : this.Cyan;
+            this.White = Console.BackgroundColor == ConsoleColor.Gray ? this.BrightWhite : this.White;
+            this.BrightRed = Console.BackgroundColor == ConsoleColor.Red ? this.Red : this.BrightRed;
+            this.BrightGreen = Console.BackgroundColor == ConsoleColor.Green ? this.Green : this.BrightGreen;
+            this.BrightYellow = Console.BackgroundColor == ConsoleColor.Yellow ? this.Yellow : this.BrightYellow;
+            this.BrightMagenta = Console.BackgroundColor == ConsoleColor.Magenta ? this.Magenta : this.BrightMagenta;
+            this.BrightCyan = Console.BackgroundColor == ConsoleColor.Cyan ? this.Cyan : this.BrightCyan;
+            this.BrightWhite = Console.BackgroundColor == ConsoleColor.White ? this.White : this.BrightWhite;
         }
 
         public string Default { get; }
 
+        public string Red { get; }
+
         public string Green { get; }
+
+        public string Yellow { get; }
 
         public string Magenta { get; }
 
@@ -27,6 +49,8 @@ namespace Bullseye.Internal
         public string White { get; }
 
         public string BrightRed { get; }
+
+        public string BrightGreen { get; }
 
         public string BrightYellow { get; }
 
