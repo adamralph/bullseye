aa764e2baba06719e3d7d4352b4115d0ae54e184
Adam Ralph <adam@adamralph.com>
2018-12-05 00:04:31 +0100

    refactor: rename IEnumerableExtensions to EnumerableExtensions

--- Bullseye/Internal/EnumerableExtensions.cs
+++ Bullseye/Internal/EnumerableExtensions.cs
@@ -3,7 +3,7 @@ namespace Bullseye.Internal
     using System.Collections.Generic;
     using System.Linq;
 
-    public static class IEnumerableExtensions
+    public static class EnumerableExtensions
     {
         public static IEnumerable<T> Sanitize<T>(this IEnumerable<T> items) where T : class =>
             (items?.Where(item => item != null) ?? Enumerable.Empty<T>());
