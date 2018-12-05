7643b3071ad35a38c54380a3bbdbda5fdeed735f
Adam Ralph <adam@adamralph.com>
2018-12-05 21:29:07 +0100

    refactor: move methods out of TargetCollectionExtensions

---
+++ Bullseye/Internal/ConsoleExtensions.cs
@@ -0,0 +1,65 @@
+namespace Bullseye.Internal
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading.Tasks;
+
+    public static class ConsoleExtensions
+    {
+        public static async Task<Logger> Initialize(this IConsole console, Options options)
+        {
+            if (options.Clear)
+            {
+                console.Clear();
+            }
+
+            var operatingSystem = OperatingSystem.Unknown;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                operatingSystem = OperatingSystem.Windows;
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                operatingSystem = OperatingSystem.Linux;
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                operatingSystem = OperatingSystem.MacOS;
+            }
+
+            if (!options.NoColor && operatingSystem == OperatingSystem.Windows)
+            {
+                await WindowsConsole.TryEnableVirtualTerminalProcessing(console.Out, options.Verbose).ConfigureAwait(false);
+            }
+
+            var isHostDetected = false;
+            if (options.Host == Host.Unknown)
+            {
+                isHostDetected = true;
+
+                if (Environment.GetEnvironmentVariable("APPVEYOR")?.ToUpperInvariant() == "TRUE")
+                {
+                    options.Host = Host.Appveyor;
+                }
+                else if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TRAVIS_OS_NAME")))
+                {
+                    options.Host = Host.Travis;
+                }
+                else if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TEAMCITY_PROJECT_NAME")))
+                {
+                    options.Host = Host.TeamCity;
+                }
+            }
+
+            var palette = new Palette(options.NoColor, options.Host, operatingSystem);
+            var log = new Logger(console, options.SkipDependencies, options.DryRun, options.Parallel, palette, options.Verbose);
+
+            await log.Version().ConfigureAwait(false);
+            await log.Verbose($"Host: {options.Host}{(options.Host != Host.Unknown ? $" ({(isHostDetected ? "detected" : "forced")})" : "")}").ConfigureAwait(false);
+            await log.Verbose($"OS: {operatingSystem}").ConfigureAwait(false);
+
+            return log;
+        }
+    }
+}
--- Bullseye/Internal/Logger.cs
+++ Bullseye/Internal/Logger.cs
@@ -5,6 +5,7 @@ namespace Bullseye.Internal
     using System.Globalization;
     using System.Linq;
     using System.Reflection;
+    using System.Text;
     using System.Threading.Tasks;
 
     public class Logger
@@ -40,6 +41,11 @@ namespace Bullseye.Internal
                 await this.console.Out.WriteLineAsync(Message(p.Verbose, $"Version: {version}")).ConfigureAwait(false);
             }
         }
+
+        public Task Usage() => this.console.Out.WriteLineAsync(this.GetUsage());
+
+        public Task Targets(TargetCollection targets, List<string> rootTargets, int maxDepth, int maxDepthToShowInputs, bool listInputs) =>
+            this.console.Out.WriteLineAsync(this.List(targets, rootTargets, maxDepth, maxDepthToShowInputs, listInputs));
 
         public Task Error(string message) => this.console.Out.WriteLineAsync(Message(p.Failed, message));
 
@@ -88,6 +94,80 @@ namespace Bullseye.Internal
 
         public Task NoInputs(string target) =>
             this.console.Out.WriteLineAsync(Message(p.Warning, "No inputs!", target, null));
+
+        private string List(TargetCollection targets, List<string> rootTargets, int maxDepth, int maxDepthToShowInputs, bool listInputs)
+        {
+            var value = new StringBuilder();
+
+            var corner = "└─";
+            var teeJunction = "├─";
+            var line = "│ ";
+
+            foreach (var rootTarget in rootTargets)
+            {
+                Append(new List<string> { rootTarget }, new Stack<string>(), true, "", 0);
+            }
+
+            return value.ToString();
+
+            void Append(List<string> names, Stack<string> seenTargets, bool isRoot, string previousPrefix, int depth)
+            {
+                if (depth > maxDepth)
+                {
+                    return;
+                }
+
+                foreach (var item in names.Select((name, index) => new { name, index }))
+                {
+                    var circularDependency = seenTargets.Contains(item.name);
+
+                    seenTargets.Push(item.name);
+
+                    try
+                    {
+                        var prefix = isRoot
+                            ? ""
+                            : $"{previousPrefix.Replace(corner, "  ").Replace(teeJunction, line)}{(item.index == names.Count - 1 ? corner : teeJunction)}";
+
+                        var isMissing = !targets.Contains(item.name);
+
+                        value.Append($"{p.Tree}{prefix}{(isRoot ? p.Target : p.Dependency)}{item.name}");
+
+                        if (isMissing)
+                        {
+                            value.AppendLine($" {p.Failed}(missing){p.Default}");
+                            continue;
+                        }
+
+                        if (circularDependency)
+                        {
+                            value.AppendLine($" {p.Failed}(circular dependency){p.Default}");
+                            continue;
+                        }
+
+                        value.AppendLine(p.Default);
+
+                        var target = targets[item.name];
+
+                        if (listInputs && depth <= maxDepthToShowInputs && target is IHaveInputs hasInputs)
+                        {
+                            foreach (var inputItem in hasInputs.Inputs.Select((input, index) => new { input, index }))
+                            {
+                                var inputPrefix = $"{prefix.Replace(corner, "  ").Replace(teeJunction, line)}{(target.Dependencies.Any() && depth + 1 <= maxDepth ? line : "  ")}";
+
+                                value.AppendLine($"{p.Tree}{inputPrefix}{p.Input}{inputItem.input}{p.Default}");
+                            }
+                        }
+
+                        Append(target.Dependencies, seenTargets, false, prefix, depth + 1);
+                    }
+                    finally
+                    {
+                        seenTargets.Pop();
+                    }
+                }
+            }
+        }
 
         private string Message(string color, string text) => $"{GetPrefix()}{color}{text}";
 
@@ -153,5 +233,43 @@ namespace Bullseye.Internal
             // minutes
             return (milliseconds / 60_000d).ToString("N0", provider) + " min";
         }
+
+        private string GetUsage() =>
+$@"{p.Label}Usage: {p.CommandLine}<command-line> {p.Option}[<options>] {p.Target}[<targets>]
+
+{p.Label}command-line: {p.Text}The command line which invokes the build targets.
+  {p.Label}Examples:
+    {p.CommandLine}build.cmd
+    {p.CommandLine}build.sh
+    {p.CommandLine}dotnet run --project targets --
+
+{p.Label}options:
+ {p.Option}-c, --clear                {p.Text}Clear the console before execution
+ {p.Option}-n, --dry-run              {p.Text}Do a dry run without executing actions
+ {p.Option}-D, --list-dependencies    {p.Text}List all (or specified) targets and dependencies, then exit
+ {p.Option}-I, --list-inputs          {p.Text}List all (or specified) targets and inputs, then exit
+ {p.Option}-T, --list-targets         {p.Text}List all (or specified) targets, then exit
+ {p.Option}-t, --list-tree            {p.Text}List all (or specified) targets and dependency trees, then exit
+ {p.Option}-N, --no-color             {p.Text}Disable colored output
+ {p.Option}-p, --parallel             {p.Text}Run targets in parallel
+ {p.Option}-s, --skip-dependencies    {p.Text}Do not run targets' dependencies
+ {p.Option}-v, --verbose              {p.Text}Enable verbose output
+ {p.Option}    --appveyor             {p.Text}Force Appveyor mode (normally auto-detected)
+ {p.Option}    --teamcity             {p.Text}Force TeamCity mode (normally auto-detected)
+ {p.Option}    --travis               {p.Text}Force Travis CI mode (normally auto-detected)
+ {p.Option}-h, --help, -?             {p.Text}Show this help, then exit (case insensitive)
+
+{p.Label}targets: {p.Text}A list of targets to run or list.
+  If not specified, the {p.Target}""default""{p.Text} target will be run, or all targets will be listed.
+
+{p.Label}Remarks:
+  {p.Text}The {p.Option}--list-xxx {p.Text}options can be combined.
+
+{p.Label}Examples:
+  {p.CommandLine}build.cmd
+  {p.CommandLine}build.cmd {p.Option}-D
+  {p.CommandLine}build.sh {p.Option}-t -I {p.Target}default
+  {p.CommandLine}build.sh {p.Target}test pack
+  {p.CommandLine}dotnet run --project targets -- {p.Option}-n {p.Target}build{p.Default}";
     }
 }
