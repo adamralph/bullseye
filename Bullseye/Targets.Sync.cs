0cff87bd507cc43e44c123416a1d0200ea647bb0
Adam Ralph <adam@adamralph.com>
2018-12-08 00:36:16 +0100

    switch to exception predicate overloads for RunTargetsAndExit*

--- Bullseye/Targets.Sync.cs
+++ Bullseye/Targets.Sync.cs
@@ -31,9 +31,12 @@ namespace Bullseye
         /// Any code which follows a call to this method will not be executed.
         /// </summary>
         /// <param name="args">The command line arguments.</param>
-        /// <param name="exceptionMessageOnly">The exception types for which to log the message only.</param>
-        public static void RunTargetsAndExit(IEnumerable<string> args, IEnumerable<Type> exceptionMessageOnly) =>
-            RunTargetsAndExitAsync(args, exceptionMessageOnly).GetAwaiter().GetResult();
+        /// <param name="messageOnly">
+        /// A predicate that is called when an exception is thrown.
+        /// Return <c>true</c> to display only the exception message instead instead of the full exception details.
+        /// </param>
+        public static void RunTargetsAndExit(IEnumerable<string> args, Func<Exception, bool> messageOnly) =>
+            RunTargetsAndExitAsync(args, messageOnly).GetAwaiter().GetResult();
 
         /// <summary>
         /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
@@ -42,51 +45,5 @@ namespace Bullseye
         /// <param name="args">The command line arguments.</param>
         public static void RunTargetsAndExit(IEnumerable<string> args) =>
             RunTargetsAndExitAsync(args).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T">The exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        public static void RunTargetsAndExit<T>(IEnumerable<string> args)
-            where T : Exception
-            => RunTargetsAndExitAsync<T>(args).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        public static void RunTargetsAndExit<T1, T2>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception
-            => RunTargetsAndExitAsync<T1, T2>(args).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T3">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        public static void RunTargetsAndExit<T1, T2, T3>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception where T3 : Exception
-            => RunTargetsAndExitAsync<T1, T2, T3>(args).GetAwaiter().GetResult();
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T3">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T4">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        public static void RunTargetsAndExit<T1, T2, T3, T4>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception where T3 : Exception where T4 : Exception
-            => RunTargetsAndExitAsync<T1, T2, T3, T4>(args).GetAwaiter().GetResult();
     }
 }
--- Bullseye/Targets.cs
+++ Bullseye/Targets.cs
@@ -31,10 +31,13 @@ namespace Bullseye
         /// Any code which follows a call to this method will not be executed.
         /// </summary>
         /// <param name="args">The command line arguments.</param>
-        /// <param name="exceptionMessageOnly">The exception types for which to log the message only.</param>
+        /// <param name="messageOnly">
+        /// A predicate that is called when an exception is thrown.
+        /// Return <c>true</c> to display only the exception message instead instead of the full exception details.
+        /// </param>
         /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
-        public static Task RunTargetsAndExitAsync(IEnumerable<string> args, IEnumerable<Type> exceptionMessageOnly) =>
-            targets.RunAndExitAsync(args, exceptionMessageOnly);
+        public static Task RunTargetsAndExitAsync(IEnumerable<string> args, Func<Exception, bool> messageOnly) =>
+            targets.RunAndExitAsync(args, messageOnly);
 
         /// <summary>
         /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
@@ -44,55 +47,5 @@ namespace Bullseye
         /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
         public static Task RunTargetsAndExitAsync(IEnumerable<string> args) =>
             RunTargetsAndExitAsync(args, default);
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T">The exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
-        public static Task RunTargetsAndExitAsync<T>(IEnumerable<string> args)
-            where T : Exception
-            => RunTargetsAndExitAsync(args, new[] { typeof(T) });
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
-        public static Task RunTargetsAndExitAsync<T1, T2>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception
-            => RunTargetsAndExitAsync(args, new[] { typeof(T1), typeof(T2) });
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T3">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
-        public static Task RunTargetsAndExitAsync<T1, T2, T3>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception where T3 : Exception
-            => RunTargetsAndExitAsync(args, new[] { typeof(T1), typeof(T2), typeof(T3) });
-
-        /// <summary>
-        /// Runs the previously specified targets and then calls <see cref="Environment.Exit(int)"/>.
-        /// Any code which follows a call to this method will not be executed.
-        /// </summary>
-        /// <typeparam name="T1">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T2">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T3">An exception type for which to log the message only.</typeparam>
-        /// <typeparam name="T4">An exception type for which to log the message only.</typeparam>
-        /// <param name="args">The command line arguments.</param>
-        /// <returns>A <see cref="Task"/> that represents the asynchronous running of the targets.</returns>
-        public static Task RunTargetsAndExitAsync<T1, T2, T3, T4>(IEnumerable<string> args)
-            where T1 : Exception where T2 : Exception where T3 : Exception where T4 : Exception
-            => RunTargetsAndExitAsync(args, new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4) });
     }
 }
