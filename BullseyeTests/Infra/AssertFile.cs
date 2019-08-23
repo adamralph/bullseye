e43bab0ec90a5f59f45bb9f8af966d38ec8ef624
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2019-08-23 12:02:32 +0200

    inverted if

--- BullseyeTests/Infra/AssertFile.cs
+++ BullseyeTests/Infra/AssertFile.cs
@@ -7,16 +7,18 @@ namespace BullseyeTests.Infra
     {
         public static void Contains(string expectedPath, string actual)
         {
-            if (actual != File.ReadAllText(expectedPath))
+            if (actual == File.ReadAllText(expectedPath))
             {
-                var actualPath = Path.Combine(
-                    Path.GetDirectoryName(expectedPath),
-                    Path.GetFileNameWithoutExtension(expectedPath) + "-actual" + Path.GetExtension(expectedPath));
-
-                File.WriteAllText(actualPath, actual);
-
-                throw new Exception($"{actualPath} does not contain the contents of {expectedPath}.");
+                return;
             }
+
+            var actualPath = Path.Combine(
+                Path.GetDirectoryName(expectedPath),
+                Path.GetFileNameWithoutExtension(expectedPath) + "-actual" + Path.GetExtension(expectedPath));
+
+            File.WriteAllText(actualPath, actual);
+
+            throw new Exception($"{actualPath} does not contain the contents of {expectedPath}.");
         }
     }
 }
