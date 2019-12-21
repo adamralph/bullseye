558ef0e6c66e437bdc382649eaa2ea2b67188167
Adam Ralph <adam@adamralph.com>
2019-12-21 12:59:24 +0000

    only push package during build if API key is present

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for package in $(find -name "*.nupkg"); do
-  echo "${0##*/}": Pushing $package...
-  dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key $1
-done
+if [[ -z "${MYGET_ADAMRALPH_CI_API_KEY}" ]]
+then
+  echo "${0##*/}: MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s)."
+else
+  for package in $(find -name "*.nupkg" | grep "test" -v); do
+    echo "${0##*/}: Pushing $package..."
+    dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key ${MYGET_ADAMRALPH_CI_API_KEY}
+  done
+fi
