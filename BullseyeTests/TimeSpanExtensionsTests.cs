abbef21ed84abe3f13898bb1512a29c276b2589a
Adam Ralph <adam@adamralph.com>
2020-06-13 19:50:09 +0200

    expanded TimeSpanExtensionsTests

--- BullseyeTests/TimeSpanExtensionsTests.cs
+++ BullseyeTests/TimeSpanExtensionsTests.cs
@@ -8,19 +8,47 @@ namespace BullseyeTests
     public class TimeSpanExtensionsTests
     {
         [Scenario]
+        [Example(null, "")]
         [Example(0.9D, "<1 ms")]
         [Example(1D, "1 ms")]
+        [Example(1.234D, "1.23 ms")]
+        [Example(123.4D, "123 ms")]
         [Example(1_000D, "1 s")]
-        [Example(119_000D, "1 m 59 s")]
-        [Example(1_000_000D, "16 m 40 s")]
-        [Example(1_000_000_000D, "16,667 m")]
-        public void Humanization(double milliseconds, string expected, TimeSpan timeSpan, string actual)
+        [Example(1_234D, "1.23 s")]
+        [Example(12_340D, "12.3 s")]
+        [Example(60_000D, "1 m")]
+        [Example(61_234D, "1 m 1 s")]
+        [Example(3_599_999D, "59 m 59 s")]
+        [Example(3_600_000D, "60 m")]
+        [Example(3_612_345D, "60 m")]
+        [Example(3_661_234D, "61 m")]
+        public void Humanization(double? milliseconds, string expected, TimeSpan? timeSpan, string actual)
+        {
+            $"Given a timespan of {milliseconds} milliseconds"
+                .x(() => timeSpan = milliseconds.HasValue ? TimeSpan.FromMilliseconds(milliseconds.Value) : (TimeSpan?)null);
+
+            $"When humanizing the timespan"
+                .x(() => actual = timeSpan.Humanize());
+
+            $"Then the result is \"{expected}\""
+                .x(() => Assert.Equal(expected, actual));
+        }
+
+        [Scenario]
+        [Example(0.9D, "<1 ms")]
+        [Example(1D, "1 ms")]
+        [Example(1.234D, "1 ms")]
+        [Example(123.4D, "123 ms")]
+        [Example(1_000D, "1.00 s")]
+        [Example(1_234D, "1.23 s")]
+        [Example(12_340D, "12.34 s")]
+        public void FixedPointHumanization(double milliseconds, string expected, TimeSpan? timeSpan, string actual)
         {
             $"Given a timespan of {milliseconds} milliseconds"
                 .x(() => timeSpan = TimeSpan.FromMilliseconds(milliseconds));
 
             $"When humanizing the timespan"
-                .x(() => actual = timeSpan.Humanize());
+                .x(() => actual = timeSpan.Humanize(true));
 
             $"Then the result is \"{expected}\""
                 .x(() => Assert.Equal(expected, actual));
