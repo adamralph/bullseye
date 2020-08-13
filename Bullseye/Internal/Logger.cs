e95d7cc6485cae420114a93f45de410a337853df
Adam Ralph <adam@adamralph.com>
2020-08-13 21:52:56 +0100

    refactor: remove redundant using in Logger

--- Bullseye/Internal/Logger.cs
+++ Bullseye/Internal/Logger.cs
@@ -7,7 +7,6 @@ namespace Bullseye.Internal
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
     using static System.Math;
