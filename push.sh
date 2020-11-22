ed6bd104cc5f5de67fb59ec032939b1325c5cd3e
Adam Ralph <adam@adamralph.com>
2020-11-22 22:16:47 +0100

    fix linting violations

--- push.sh
+++ push.sh
@@ -5,8 +5,8 @@ if [[ -z "${MYGET_ADAMRALPH_CI_API_KEY}" ]]
 then
   echo "${0##*/}: MYGET_ADAMRALPH_CI_API_KEY is empty or not set. Skipped pushing package(s)."
 else
-  for package in $(find -name "*.nupkg" | grep "test" -v); do
+  for package in $(find . -name "*.nupkg" | grep "test" -v); do
     echo "${0##*/}: Pushing $package..."
-    dotnet nuget push $package --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key ${MYGET_ADAMRALPH_CI_API_KEY}
+    dotnet nuget push "$package" --source https://www.myget.org/F/adamralph-ci/api/v2/package --api-key "${MYGET_ADAMRALPH_CI_API_KEY}"
   done
 fi
