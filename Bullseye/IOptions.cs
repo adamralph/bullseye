fc6c0eaad3e9264b6bec7951c6361a9060f3224b
Adam Ralph <adam@adamralph.com>
2021-11-06 15:35:21 +0100

    accept IOptions instead of Options

---
+++ Bullseye/IOptions.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+
+namespace Bullseye
+{
+    /// <summary>
+    /// The options to use when running or listing targets.
+    /// </summary>
+    public interface IOptions
+    {
+        /// <summary>
+        /// Gets a value indicating whether the console should be cleared before execution.
+        /// </summary>
+        bool Clear { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to do a dry run without executing actions.
+        /// </summary>
+        bool DryRun { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to list all (or specified) targets and then exit.
+        /// </summary>
+        bool ListDependencies { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to list all (or specified) targets and inputs and then exit.
+        /// </summary>
+        bool ListInputs { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to list all (or specified) targets and then exit.
+        /// </summary>
+        bool ListTargets { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to list all (or specified) targets and dependency trees and then exit.
+        /// </summary>
+        bool ListTree { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to disable colored output.
+        /// </summary>
+        bool NoColor { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to disable extended characters.
+        /// </summary>
+        bool NoExtendedChars { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to run targets in parallel.
+        /// </summary>
+        bool Parallel { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to run target's dependencies.
+        /// </summary>
+        bool SkipDependencies { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to enable verbose output.
+        /// </summary>
+        bool Verbose { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to force the mode for a specific host environment (normally auto-detected).
+        /// If the value is set to <c>null</c>, then no mode is forced.
+        /// </summary>
+        Host? Host { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to show help and exit.
+        /// </summary>
+        bool ShowHelp { get; }
+
+        /// <summary>
+        /// Gets the list of unknown options represented by the named values passed to the constructor.
+        /// </summary>
+        IReadOnlyList<string> UnknownOptions { get; }
+    }
+}
