fe5b6d0679930b0069d7097e952ef06e0cfebff3
Adam Ralph <adam@adamralph.com>
2021-11-09 14:52:36 +0100

    address warning about dotnet run -p option

--- build.cmd
+++ build.cmd
@@ -8,34 +8,34 @@ dotnet test --configuration Release --no-build --nologo %1 %2 || goto :error
 
 echo %~nx0: Smoke testing...
 @echo On
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --help || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --list-targets || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --list-dependencies || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --list-inputs || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --list-dependencies --list-inputs || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --list-tree --list-inputs || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --parallel || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --dry-run || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --skip-dependencies || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --dry-run --skip-dependencies || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- --verbose || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- -h --verbose || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- -h --verbose --no-color || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --help || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --list-targets || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --list-dependencies || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --list-inputs || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --list-dependencies --list-inputs || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --list-tree --list-inputs || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --parallel || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --dry-run || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --skip-dependencies || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --dry-run --skip-dependencies || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- --verbose || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- -h --verbose || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- -h --verbose --no-color || goto :error
 
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- large-graph --verbose --parallel || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- large-graph --verbose --parallel || goto :error
 
-dotnet run -c Release --no-build -p BullseyeSmokeTester.DragonFruit  -- --help || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester.DragonFruit  -- --foo bar --verbose --targets build || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.DragonFruit  -- --help || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.DragonFruit  -- --foo bar --verbose --targets build || goto :error
 
-dotnet run -c Release --no-build -p BullseyeSmokeTester.CommandLine  -- --help || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester.CommandLine  -- --foo bar --verbose build || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.CommandLine  -- --help || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.CommandLine  -- --foo bar --verbose build || goto :error
 
-dotnet run -c Release --no-build -p BullseyeSmokeTester.McMaster     -- --help || goto :error
-dotnet run -c Release --no-build -p BullseyeSmokeTester.McMaster     -- --foo bar --verbose build || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.McMaster     -- --help || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester.McMaster     -- --foo bar --verbose build || goto :error
 
 set NO_COLOR=1
-dotnet run -c Release --no-build -p BullseyeSmokeTester -- -h --verbose || goto :error
+dotnet run -c Release --no-build --project BullseyeSmokeTester -- -h --verbose || goto :error
 
 @echo Off
 
