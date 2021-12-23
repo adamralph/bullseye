0108c5dea6251c699bdda7cafa42b2a3c831f924
Adam Ralph <adam@adamralph.com>
2021-12-23 14:35:22 +0100

    update smoke tests to latest System.CommandLine packages

--- BullseyeSmokeTester.CommandLine/Program.cs
+++ BullseyeSmokeTester.CommandLine/Program.cs
@@ -1,14 +1,11 @@
 #pragma warning disable CA1812 // https://github.com/dotnet/roslyn-analyzers/issues/5628
 using System.CommandLine;
-using System.CommandLine.Invocation;
 using System.Linq;
 using Bullseye;
 using static Bullseye.Targets;
 
-var cmd = new RootCommand()
-{
-    new Option<string>( new[] { "--foo", "-f" }, "A value used for something."),
-};
+var foo = new Option<string>(new[] { "--foo", "-f" }, "A value used for something.");
+var cmd = new RootCommand() { foo, };
 
 // translate from Bullseye to System.CommandLine
 cmd.Add(new Argument("targets") { Arity = ArgumentArity.ZeroOrMore, Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed." });
@@ -17,14 +14,14 @@ foreach (var option in Options.Definitions)
     cmd.Add(new Option(option.Aliases.ToArray(), option.Description));
 }
 
-cmd.Handler = CommandHandler.Create<string>(async foo =>
+cmd.SetHandler(async () =>
 {
     // translate from System.CommandLine to Bullseye
     var cmdLine = cmd.Parse(args);
     var targets = cmdLine.CommandResult.Tokens.Select(token => token.Value);
-    var options = new Options(Options.Definitions.Select(o => (o.LongName, cmdLine.ValueForOption<bool>(o.LongName))));
+    var options = new Options(Options.Definitions.Select(d => (d.LongName, cmdLine.GetValueForOption<bool>(cmd.Options.Single(o => o.HasAlias(d.LongName))))));
 
-    Target("build", async () => await System.Console.Out.WriteLineAsync($"foo = {foo}"));
+    Target("build", async () => await System.Console.Out.WriteLineAsync($"foo = {cmdLine.GetValueForOption(foo)}"));
 
     Target("default", DependsOn("build"));
 
