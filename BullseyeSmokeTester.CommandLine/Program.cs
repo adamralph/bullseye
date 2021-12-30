70d5084d47a62b677bf7e75a857ed2e97fae1168
Adam Ralph <adam@adamralph.com>
2021-12-30 19:06:58 +0100

    remove OptionDefinition type

--- BullseyeSmokeTester.CommandLine/Program.cs
+++ BullseyeSmokeTester.CommandLine/Program.cs
@@ -9,9 +9,9 @@ var cmd = new RootCommand() { foo, };
 
 // translate from Bullseye to System.CommandLine
 cmd.Add(new Argument("targets") { Arity = ArgumentArity.ZeroOrMore, Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed." });
-foreach (var option in Options.Definitions)
+foreach (var (aliases, description) in Options.Definitions)
 {
-    cmd.Add(new Option(option.Aliases.ToArray(), option.Description));
+    cmd.Add(new Option(aliases.ToArray(), description));
 }
 
 cmd.SetHandler(async () =>
@@ -19,7 +19,7 @@ cmd.SetHandler(async () =>
     // translate from System.CommandLine to Bullseye
     var cmdLine = cmd.Parse(args);
     var targets = cmdLine.CommandResult.Tokens.Select(token => token.Value);
-    var options = new Options(Options.Definitions.Select(d => (d.LongName, cmdLine.GetValueForOption<bool>(cmd.Options.Single(o => o.HasAlias(d.LongName))))));
+    var options = new Options(Options.Definitions.Select(d => (d.Aliases[0], cmdLine.GetValueForOption<bool>(cmd.Options.Single(o => o.HasAlias(d.Aliases[0]))))));
 
     Target("build", async () => await System.Console.Out.WriteLineAsync($"foo = {cmdLine.GetValueForOption(foo)}"));
 
