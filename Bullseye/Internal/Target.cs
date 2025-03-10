a709f29afc5fd17e55a338b39fe4b7f48ace8a23
Adam Ralph <adam@adamralph.com>
2025-03-10 11:08:55 +0100

    limit parallel targets to logical processor count

--- Bullseye/Internal/Target.cs
+++ Bullseye/Internal/Target.cs
@@ -8,7 +8,7 @@ public class Target(string name, string description, IEnumerable<string> depende
 
     public IReadOnlyCollection<string> Dependencies { get; } = [.. dependencies];
 
-    public virtual Task RunAsync(bool dryRun, bool parallel, Output output, Func<Exception, bool> messageOnly, IReadOnlyCollection<Target> dependencyPath) => output.Succeeded(this, dependencyPath, TimeSpan.Zero);
+    public virtual Task RunAsync(bool dryRun, bool parallel, SemaphoreSlim parallelTargets, Output output, Func<Exception, bool> messageOnly, IReadOnlyCollection<Target> dependencyPath) => output.Succeeded(this, dependencyPath, TimeSpan.Zero);
 
     public override string ToString() => this.Name;
 }
