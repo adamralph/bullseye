29630e2d7e6c397efda0d00f78da2f842557fc66
Adam Ralph <adam@adamralph.com>
2025-12-13 23:06:08 +0100

    refactor smoke testers

---
+++ BullseyeSmokeTester.McMaster/Extensions.cs
@@ -0,0 +1,24 @@
+using Bullseye;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace BullseyeSmokeTester.McMaster;
+
+internal static class Extensions
+{
+    extension(CommandLineApplication app)
+    {
+        internal CommandArgument TargetsArgument() => app.Argument(
+            "targets",
+            "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.",
+            multipleValues: true);
+
+        internal IReadOnlyList<CommandOption> BullseyeOptions() => [.. Options.Definitions.Select(
+            d => app.Option<bool>(string.Join("|", d.Aliases), d.Description, CommandOptionType.NoValue))];
+    }
+
+    extension(IEnumerable<CommandOption> options)
+    {
+        internal Options GetOptions() => new(Options.Definitions.Select(d =>
+            (d.Aliases[0], options.Single(o => d.Aliases.Contains($"--{o.LongName}")).HasValue())));
+    }
+}
--- BullseyeSmokeTester.McMaster/Program.cs
+++ BullseyeSmokeTester.McMaster/Program.cs
@@ -1,29 +1,21 @@
-using Bullseye;
+using BullseyeSmokeTester.McMaster;
 using McMaster.Extensions.CommandLineUtils;
 using static Bullseye.Targets;
 
-using var app = new CommandLineApplication { UsePagerForHelpText = false, };
-app.HelpOption();
+using var app = new CommandLineApplication();
+
+_ = app.HelpOption();
 var foo = app.Option<string>("-f|--foo <foo>", "A value used for something.", CommandOptionType.SingleValue);
+var targets = app.TargetsArgument();
+var options = app.BullseyeOptions();
 
-// translate from Bullseye to McMaster.Extensions.CommandLineUtils
-app.Argument("targets", "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.", true);
-foreach (var (aliases, description) in Options.Definitions)
+app.OnExecuteAsync(async _ =>
 {
-    _ = app.Option(string.Join("|", aliases), description, CommandOptionType.NoValue);
-}
+    Target("build", () => Console.Out.WriteLineAsync($"{nameof(foo)} = {foo.Value()}"));
 
-app.OnExecuteAsync(_ =>
-{
-    // translate from McMaster.Extensions.CommandLineUtils to Bullseye
-    var targets = app.Arguments[0].Values.OfType<string>();
-    var options = new Options(Options.Definitions.Select(d => (d.Aliases[0], app.Options.Single(o => d.Aliases.Contains($"--{o.LongName}")).HasValue())));
+    Target("default", dependsOn: ["build",]);
 
-    Target("build", () => Console.Out.WriteLineAsync($"foo = {foo.Value()}"));
-
-    Target("default", dependsOn: ["build"]);
-
-    return RunTargetsAndExitAsync(targets, options);
+    await RunTargetsAndExitAsync(targets.Values!, options.GetOptions());
 });
 
 return await app.ExecuteAsync(args);
